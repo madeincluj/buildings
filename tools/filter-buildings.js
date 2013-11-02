3f57f07ac86e6284283d67da4ea350f0185c0680
Dan Burzo <danburzo@gmail.com>
2013-11-02 16:24:38 +0200

    Changed way we filter buildings

--- tools/filter-buildings.js
+++ tools/filter-buildings.js
@@ -4,15 +4,23 @@ var yaml = require('js-yaml');
 var file = fs.readFileSync('tools/selected-buildings.yaml', 'utf8');
 var selected_buildings = yaml.load(file).buildings;
 
+var bldg = {};
+selected_buildings.forEach(function(building) {
+	bldg[building.id] = building.name;
+});
+
 var all_buildings = JSON.parse(fs.readFileSync('json/buildings.json', 'utf8'));
 
 var selected_features = all_buildings.features.filter(function(feature) {
-	return selected_buildings.indexOf(feature.properties.id) > -1;
+	return bldg[feature.properties.id];
 });
 
 var geojson = {
 	type: 'FeatureCollection',
-	features: selected_features
+	features: selected_features.map(function(feature) {
+		feature.properties.name = bldg[feature.properties.id];
+		return feature;
+	})
 };
 
 fs.writeFileSync('json/selected-buildings.json', JSON.stringify(geojson, null, 2));
